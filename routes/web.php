53898bd2929ffe942da8c64f5ed367af00c168bb
lucasabella <lucasagustinabella@gmail.com>
2024-10-24 23:17:39 -0300

    hotfix getall users metod

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\UserController;
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users', [UserController::class, 'getAll']);
+Route::get('/users', [UserController::class, 'getAll']); //index recomendado
 
 
 
