fd61ca1bf9a25c2df93e13741cb03d128233b74b
lucasagustinAbella <lucasagustinabella@gmail.com>
2024-10-25 11:22:54 -0300

    login no auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function login(){
-        
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+        // if (!$user || !Hash::check($request->password, $user->password)) {
+        //     return response()->json(['error' => 'Unauthorized'], 401);
+        // }
+
+        return response()->json(['message' => 'Login successful', 'user' => $user]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,15 @@ use App\Http\Controllers\UserController;
 
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/users/login', [AuthController::class, 'login']); 
+Route::post('/register', [AuthController::class, 'register']); 
+
 Route::get('/users', [UserController::class, 'getAll']); //index recomendado
 Route::get('/users/{id}', [UserController::class, 'getById']); 
 Route::post('/users/{id}', [UserController::class, 'softDelete']);  //borrado logico
 Route::put('/users/{id}', [UserController::class, 'updateById']); 
 
-Route::get('/login', [AuthController::class, 'login']); 
-Route::post('/register', [AuthController::class, 'register']); 
 
 
 
@@ -19,7 +21,7 @@ Route::post('/register', [AuthController::class, 'register']);
 
 
 
-//default
+//default laravel
 Route::get('/', function () {
     return view('welcome');
 });
